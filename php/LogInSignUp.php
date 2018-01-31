cd28f7e57829e23dbf487d31844c9be65b104969
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2018-01-31 11:29:56 +0100

    changement wamp

--- php/LogInSignUp.php
+++ php/LogInSignUp.php
@@ -34,7 +34,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" type="text/css" href="css/styleLogs.css">
+    <link rel="stylesheet" type="text/css" href="../css/styleLogs.css">
     <title>Log In - Sign Up</title>
 </head>
 <body>
