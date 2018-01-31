2638867678911ba2d3b2d9d0ce903909d60cbcd5
Cyrille Carpentier <cyrille.carpentier@imie.fr>
2018-01-31 10:26:23 +0100

    dossier php

---
+++ php/fonctions.php
@@ -0,0 +1,56 @@
+<?php
+
+
+//choppe la BDD
+    function getPDO(){
+        return new PDO('mysql:host=localhost;dbname=voyageinterg;charset=utf8', 'root', '');
+    }
+
+//il faudra peut etre enlever les '' autour de sha256
+    function cryptage($mail, $passwd){
+        $passcrypt = $mail.KEY.KEY.$passwd.KEY;
+        return hash('sha256', $passcrypt, false);
+    }
+
+//Vérifie que l'email existe et que le mot de passe correspond
+// return 1 = utilisateur existant et bon logs
+    function verifConnexion($mail, $passwd){
+        $bdd = getPDO();
+        $requete = $bdd->prepare('SELECT * FROM users WHERE user_mail =:mail and user_password =:pass');
+        $requete-> bindParam(':mail', $mail);
+        $requete-> bindParam(':pass', $passwd);
+        $requete-> execute();
+        $verifmail = $requete->fetchAll(PDO::FETCH_ASSOC);      
+        if(count($verifmail) > 0){
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+
+//Ecris dans la bdd si identifiants allowed (inscription)
+    function writeLog($mail, $passCrypt, $last_name, $first_name){
+        $bdd = getPDO();
+        $requete = $bdd->prepare('INSERT INTO users (user_mail, user_password, user_last_name, user_firt_name) VALUES (:mail, :passcrypt, :last_name, :first_name);');
+        $requete-> bindParam(':mail', $mail);
+        $requete-> bindParam(':passCrypt', $passCrypt);    
+        $requete-> bindParam(':last_name', $last_name);
+        $requete-> bindParam(':first_name', $first_name);
+        $requete->execute();      
+    }
+
+//Vérifie si le mail n'est pas déjà utilisé
+    function verifInscription($mail){
+        $bdd = getPDO();
+        $isValid = 1;
+        $mailtemp = $bdd->query('SELECT user_mail FROM users');
+        $verifmail = $mailtemp->fetchAll(PDO::FETCH_ASSOC);  
+        foreach($verifmail as $value){
+            if($mail == $value['mail']){
+                return $isValid = 2;
+            }               
+        }
+        return $isValid;
+    }
+
+?>
