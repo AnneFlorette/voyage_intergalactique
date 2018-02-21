4f368c6b203290f850c0e1a2f365aea185d9f91f
AnneFlorette <anneflorecuisset@gmail.com>
2018-02-21 15:11:36 +0100

    new page logs

---
+++ logInSignUp.php
@@ -0,0 +1,110 @@
+<?php
+    include 'functions.php';
+    session_start();
+
+    $allowedSignUp = true;
+
+//verif inscription
+    if (isset($_POST['mailSignUp']) && htmlspecialchars($_POST['mailSignUp']) != "" &&
+        isset($_POST['passwdSignUp']) && htmlspecialchars($_POST['passwdSignUp']) != "" &&
+        isset($_POST['lastName']) && htmlspecialchars($_POST['lastName']) != "" &&
+        isset($_POST['firstName']) && htmlspecialchars($_POST['firstName']) != "" &&
+        htmlspecialchars($_POST['passwdSignUpVerif']) == htmlspecialchars($_POST['passwdSignUp'])) {
+        $mail = htmlspecialchars($_POST['mailSignUp']);
+        $passwd = htmlspecialchars($_POST['passwdSignUp']);
+        $lastName = htmlspecialchars($_POST['lastName']);
+        $firstName = htmlspecialchars($_POST['firstName']);
+
+        $allowedSignUp = verifInscription($mail);
+
+        if ($allowedSignUp == true){
+            $passCrypt = cryptage($mail, $passwd);
+            writeLog($mail, $passCrypt, $lastName, $firstName);
+            setcookie('logIn', $mail, time() + 30*24*3600, null, null, false ,true);
+            //header('location: LogInSignUp.html');
+        }
+    }
+
+    $allowedLogIn = false;
+//verif connexion
+    if (isset($_POST['mailLogIn']) && htmlspecialchars($_POST['mailLogIn']) != "" &&
+        isset($_POST['passwdLogIn']) && htmlspecialchars($_POST['passwdLogIn']) != "") {
+        $mail = htmlspecialchars($_POST['mailLogIn']);
+        $passwd = htmlspecialchars($_POST['passwdLogIn']);
+        $logCrypt = cryptage($mail, $passwd);
+        $allowedLogin = verifConnexion($mail, $logCrypt);
+        if($allowedLogin == true){
+            $ID = getID($mail);
+            $_SESSION['ID'] = $ID;
+            setcookie('logIn', $mail, time() + 30*24*3600, null, null, false ,true);
+        }
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/styleLogs.css">
+    <title>Log In - Sign Up</title>
+</head>
+<body>
+        <nav>
+            <ul>
+                <a href="index.php"><li id="home">Home</li></a>
+                <a href="ourCompany.php"><li id="company">Our Company</li></a>
+                <a href="ourDestinations.php"><li id="log">Our Destinations</li></a>
+            </ul>
+        </nav>
+        <div id="titleBox">
+            <a href="index.php"><h1>Far Away</h1></a>
+        </div>
+    <div id="logs">
+        <div id="signUp">
+            <form action="" method="POST">
+                <h3>Sign Up</h3>
+                <label for="firstName">First Name</label>
+                <br>
+                <input type="text" name="firstName" autofocus required>
+                <br>
+                <label for="lastName">Last Name</label>
+                <br>
+                <input type="text" name="lastName" required>
+                <br>
+                <label for="mailSignUp">Email</label>
+                <br>
+                <input type="email" name="mailSignUp" required>
+                <br>
+                <label for="passwdSignUp">Password</label>
+                <br>
+                <input type="password" name="passwdSignUp" required>
+                <br>
+                <label for="passwdSignUpVerif">Password Check</label>
+                <br>
+                <input type="password" name="passwdSignUpVerif" required>
+                <br>
+                <input type="submit" value="Sign Up" class="submit">
+                <br>
+            </form>
+        </div>
+        <div id="logIn">
+            <form action="" method="POST">
+                <h3>Log In</h3>
+                <label for="mailLogIn">Email</label>
+                <br>
+                <input type="email" name="mailLogIn" required>
+                <br>
+                <label for="passwdLogIn">Password</label>
+                <br>
+                <input type="password" name="passwdLogIn" required>
+                <br>
+                <input type="submit" value="Log In" class="submit">
+                <br>
+            </form>          
+        </div>
+    </div>
+
+</body>
+</html>
