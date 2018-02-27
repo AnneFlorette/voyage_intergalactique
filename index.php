c8e70488c0db33ad5bd5f7d3575e4427b1ce98cd
Trobibot <hugomouchel21@gmail.com>
2018-02-27 16:42:29 +0100

    Index: adding mc solar systeme

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script src="https://cdn.jsdelivr.net/npm/vue"></script>
     <link rel="stylesheet" type="text/css" href="css/styleIndex.css">
+    <link rel="stylesheet" href="css/styleWaiting.css">
     <title>Far Away</title>
 </head>
 <body>
@@ -21,10 +22,33 @@
         <h1>Far Away</h1>
         <p id="caption">Go deeper in Space</p>
     </div>
+    <ul class="wrapper">
+        <div class="sun">
+            <div class="star"></div>
+        </div>
+        <div class="mercury">
+            <div class="planet">
+            <div class="shadow"></div>
+            </div>
+        </div>
+        <div class="venus">
+            <div class="planet">
+            <div class="shadow"></div>
+            </div>
+        </div>
+        <div class="earth">
+            <div class="planet"><div class="shadow"></div></div>
+        </div>
+        <div class="mars">
+            <div class="planet"><div class="shadow"></div></div>
+        </div>
+        <div class="jupiter">
+            <div class="planet"><div class="shadow"></div></div>
+        </div>
+    </ul>
     <div id="buttonBox">
         <a href="ourDestinations.php"><button id="discoverTrip">Our Destinations</button></a>
         <a href="ourCompany.php"><button id="ourCompany">Our Company</button></a>
     </div>
-    
 </body>
 </html>
